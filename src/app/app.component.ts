1b0da83240e9bcdfff130e0444dc98712910620d
Louis Nguyen <tho.it@impc.vn>
2023-01-29 06:57:16 +0000

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,23 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import faker from 'faker';
+import { of } from "rxjs";
+
+const getImage = () => {
+  const arr = [
+    {
+      src: "https://i.postimg.cc/tgMnbr02/osb-demo-1280x720.jpg",
+      width: 1280,
+      height: 720
+    },
+    {
+      src: "https://i.postimg.cc/SR0X2Jx9/osb-demo-1280x990.jpg",
+      width: 1280,
+      height: 990
+    },
+  ]
+
+  return arr[~~(Math.random() * arr.length)];
+}
 
 @Component({
   selector: 'app-root',
@@ -6,5 +25,36 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'visual-information-retrieval';
+  entries: Array<any> = [];
+
+  constructor() {
+    this.create()
+  }
+
+  create() {
+    for (var i = 1; i <= 12; i++) {
+      this.entries.push({
+        id: faker.datatype.number(),
+        title: faker.lorem.words(5),
+        body: faker.lorem.sentences(3),
+        image: (i % 5 === 0) ? '' : getImage()
+      });
+    }
+  }
+
+  add() {
+    let obj = {
+      image: getImage(),
+      id: faker.datatype.number(),
+      title: faker.lorem.words(5),
+      body: faker.lorem.sentences(3),
+    };
+
+    this.entries.unshift(obj);
+  }
+
+  getEntries() {
+    return of(this.entries);
+  }
+
 }
