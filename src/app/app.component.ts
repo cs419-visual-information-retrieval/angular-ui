9695c43fc0d99ebb36c0447832215d803383154c
Louis Nguyen <tho.it@impc.vn>
2023-01-31 03:53:22 +0000

    temporary complete UI

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
+import { HttpClient } from "@angular/common/http";
 import { Component } from "@angular/core";
-import faker from 'faker';
-import { of } from "rxjs";
+import { ImageCroppedEvent } from "ngx-image-cropper";
+import { first } from "rxjs";
 
 const getImage = () => {
   const arr = [
@@ -19,42 +20,128 @@ const getImage = () => {
   return arr[~~(Math.random() * arr.length)];
 }
 
+function b64toBlob(b64Data: string, contentType: string, sliceSize: number = 512) {
+  contentType = contentType || '';
+  sliceSize = sliceSize || 512;
+
+  var byteCharacters = atob(b64Data);
+  var byteArrays = [];
+
+  for (var offset = 0; offset < byteCharacters.length; offset += sliceSize) {
+    var slice = byteCharacters.slice(offset, offset + sliceSize);
+
+    var byteNumbers = new Array(slice.length);
+    for (var i = 0; i < slice.length; i++) {
+      byteNumbers[i] = slice.charCodeAt(i);
+    }
+
+    var byteArray = new Uint8Array(byteNumbers);
+
+    byteArrays.push(byteArray);
+  }
+
+  var blob = new Blob(byteArrays, { type: contentType });
+  return blob;
+}
+
+
+function imageToBlob(base64: string) {
+  // Split the base64 string in data and contentType
+  let block = base64.split(";");
+  // Get the content type of the image
+  let contentType = block[0].split(":")[1];// In this case "image/gif"
+  // get the real base64 content of the file
+  let realData = block[1].split(",")[1];// In this case "R0lGODlhPQBEAPeoAJosM...."
+
+  // Convert it to a blob to upload
+  return b64toBlob(realData, contentType);
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+
   entries: Array<any> = [];
+  imageChangedEvent: any = '';
+  croppedImage: any = '';
+  status: string = '';
+  requestTime: number = 0;
 
-  constructor() {
-    this.create()
+  constructor(private http: HttpClient) {
   }
 
-  create() {
-    for (var i = 1; i <= 12; i++) {
-      this.entries.push({
-        id: faker.datatype.number(),
-        title: faker.lorem.words(5),
-        body: faker.lorem.sentences(3),
-        image: (i % 5 === 0) ? '' : getImage()
-      });
-    }
+  fileChangeEvent(event: any): void {
+    console.log(event);
+    this.imageChangedEvent = event;
   }
 
-  add() {
-    let obj = {
-      image: getImage(),
-      id: faker.datatype.number(),
-      title: faker.lorem.words(5),
-      body: faker.lorem.sentences(3),
-    };
-
-    this.entries.unshift(obj);
+  imageCropped(event: ImageCroppedEvent) {
+    this.croppedImage = event.base64;
   }
 
-  getEntries() {
-    return of(this.entries);
+
+  drag() {
+    this.status = 'draging drag-box';
+  }
+
+  drop() {
+    this.status = 'drag-box';
+  }
+
+  buildFormData() {
+    const formData = new FormData();
+    formData.append('file', imageToBlob(this.croppedImage), 'cropped-image.jpeg');
+    return formData;
+  }
+
+  inMemorySearch() {
+    let startFrom = new Date().getTime();
+    this.http.post<{
+      path: string,
+      distance: number
+    }[]>('http://127.0.0.1:5000', this.buildFormData(), {})
+      .pipe(first())
+      .subscribe(res => {
+        this.requestTime = new Date().getTime() - startFrom;
+        this.entries = res.map(hit => {
+          return {
+            image: `http://cdn.open.impc.vn/${hit.path}`,
+            title: hit.path.split("/").slice(3).join("/"),
+            distance: hit.distance
+          }
+        });
+
+      })
+
+  }
+
+  elasticSearch() {
+    let startFrom = new Date().getTime();
+    this.http.post<{
+      hits: {
+        hits: {
+          _score: number,
+          _source: {
+            path: string
+          }
+        }[],
+      }
+    }>('http://127.0.0.1:5000?type=es', this.buildFormData(), {})
+      .pipe(first())
+      .subscribe(res => {
+        this.requestTime = new Date().getTime() - startFrom;
+        this.entries = res.hits.hits.map(hit => {
+          return {
+            image: `http://cdn.open.impc.vn/${hit._source.path}`,
+            title: hit._source.path.split("/").slice(3).join("/"),
+            distance: hit._score
+          }
+        });
+      })
+
   }
 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { HttpClientModule } from '@angular/common/http';
 import { NgxFlexMasonryGridModule } from "@offensichtbar-codestock/ngx-flex-masonry-grid";
+import { ImageCropperModule } from 'ngx-image-cropper';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
@@ -12,7 +14,9 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgxFlexMasonryGridModule
+    NgxFlexMasonryGridModule,
+    ImageCropperModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
